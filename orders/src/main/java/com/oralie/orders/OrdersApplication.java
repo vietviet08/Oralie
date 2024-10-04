da0c84e9a1ceb83854cbb3777da670964dfc9458
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-10-04 21:52:16 +0700

    init module orders service

---
+++ orders/src/main/java/com/oralie/orders/OrdersApplication.java
@@ -0,0 +1,42 @@
+package com.oralie.orders;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+//@EnableConfigurationProperties(OrdersContactDto.class)
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Product microservice REST API Documentation",
+                description = "Oralie Orders microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Viet Quoc",
+                        email = "vietnq23ceb@vku.udn.vn",
+                        url = "github.com/vietviet08"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "github.com/vietviet08"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Oralie Orders microservice REST API Documentation",
+                url = "https://www.oralie.com.vn/swagger-ui.html"
+        )
+)
+public class OrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class, args);
+    }
+
+}
