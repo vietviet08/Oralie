1f9900b15d61fdc4487d447b0f319cc332803287
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-10-29 05:18:17 +0700

    29-10-24:12

--- social/src/main/java/com/oralie/social/SocialApplication.java
+++ social/src/main/java/com/oralie/social/SocialApplication.java
@@ -1,6 +1,5 @@
 package com.oralie.social;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocialApplication {
 
     public static void main(String[] args) {
-        Dotenv.configure().load();
         SpringApplication.run(SocialApplication.class, args);
     }
 
