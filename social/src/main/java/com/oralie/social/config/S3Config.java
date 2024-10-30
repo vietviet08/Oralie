e3015cffceb02758482b1b039a822ed19dfbc27b
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-10-30 09:30:10 +0700

    clean the workflow

--- social/src/main/java/com/oralie/social/config/S3Config.java
+++ social/src/main/java/com/oralie/social/config/S3Config.java
@@ -5,23 +5,17 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource("classpath:application-dev.yml")
 public class S3Config {
 
     @Value("${aws.credentials.accessKey}")
-//    @Value("${AWS_ACCESS_KEY}")
     private String awsAccessKey;
 
     @Value("${aws.credentials.secretKey}")
-//    @Value("${AWS_SECRET_KEY}")
     private String awsSecretKey;
 
     @Bean
