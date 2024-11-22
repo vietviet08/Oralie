4d53433c4144dc9c7705d79c55a979331326c2ff
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-11-22 18:59:10 +0700

    implement

--- inventory/src/main/java/com/oralie/inventory/service/InventoryService.java
+++ inventory/src/main/java/com/oralie/inventory/service/InventoryService.java
@@ -1,6 +1,11 @@
 package com.oralie.inventory.service;
 
+import com.oralie.inventory.dto.request.InventoryRequest;
+import com.oralie.inventory.dto.response.ProductBaseResponse;
+
 public interface InventoryService {
+
     void addProductToWareHouse(Long wareHouseId, Long productId, int quantity);
-    
+
+    ProductBaseResponse updateProductQuantity(InventoryRequest inventoryRequest);
 }
--- inventory/src/main/java/com/oralie/inventory/service/impl/InventoryServiceImpl.java
+++ inventory/src/main/java/com/oralie/inventory/service/impl/InventoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.oralie.inventory.service.impl;
 
+import com.oralie.inventory.dto.request.InventoryRequest;
+import com.oralie.inventory.dto.response.ProductBaseResponse;
 import com.oralie.inventory.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +11,13 @@ import org.springframework.stereotype.Service;
 public class InventoryServiceImpl implements InventoryService {
 
 
+    @Override
+    public void addProductToWareHouse(Long wareHouseId, Long productId, int quantity) {
+        
+    }
 
+    @Override
+    public ProductBaseResponse updateProductQuantity(InventoryRequest inventoryRequest) {
+        return null;
+    }
 }
