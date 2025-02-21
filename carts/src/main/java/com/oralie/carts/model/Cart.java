17efb33afd965d42a0e9321629061fc500a41926
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-02-21 19:45:07 +0700

    feat: 🎸 Dockerfile update
    update for Docker file accounts, carts service

--- carts/src/main/java/com/oralie/carts/model/Cart.java
+++ carts/src/main/java/com/oralie/carts/model/Cart.java
@@ -1,13 +1,25 @@
 package com.oralie.carts.model;
 
+import java.util.List;
+import java.util.Objects;
+
 import com.oralie.carts.dto.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -19,31 +31,33 @@ import java.util.*;
 @Builder
 public class Cart extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String userId;
+  @Column(nullable = false)
+  private String userId;
 
-    private int quantity;
+  private int quantity;
 
-    private Double totalPrice;
+  private Double totalPrice;
 
-    @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<CartItem> cartItems  = new ArrayList<>();
+  @OneToMany(mappedBy = "cart", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private List<CartItem> cartItems;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CartItem cartItem = (CartItem) o;
-        return Objects.equals(id, cartItem.getId());
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    CartItem cartItem = (CartItem) o;
+    return Objects.equals(id, cartItem.getId());
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 
 }
